0c5df1887aee55908f1d37d463897bbf6cabc4f4
catchmentsci <matthew.perks@ncl.ac.uk>
2021-03-15 10:55:32 +0000

    added input for lat/long and time diff between video and level obs

--- KLT_AddLevelButtonPushed.m
+++ KLT_AddLevelButtonPushed.m
@@ -38,18 +38,37 @@ pause(0.01);
 app.ListBox.scroll('bottom');
 
 try
+    
+    prompt      = ['Maximum allowable time difference between level observation and video capture? (mins)'];
+    dlgtitle    = 'Query';
+    definput    = num2str(10); % default value
+    app.thresh  = str2num(cell2mat(inputdlg(prompt,dlgtitle,[1 60],{definput})));
+    mins        = 0.0417./60; % 0.0417 is one hour 6.9444e-04
+    app.thresh  = app.thresh.*mins;
+    
     for b = 1:length(riverLevelTimesNum)
-        [Idx,D] = knnsearch(riverLevelTimesNum(b),app.videoDatesFormattedNum);
-        [M, I] = min(D);
-        if M < 0.0418./6 % difference of less than 10 minutes
+        
+        [~,D]     = knnsearch(riverLevelTimesNum(b),app.videoDatesFormattedNum);
+        [M, I]      = min(D);
+        
+        if M < app.thresh % difference of less than 10 minutes
+            
+            if isempty(app.location)
+                prompt          = {'Define latitude (decimal degrees)'; 'Define longitude (decimal degrees)'};
+                dlgtitle        = 'Define lat and long position of the camera to filter by daytime hours';
+                definput        = {'50.479391', '-3.762149'} ; % default value
+                app.location    = str2num(cell2mat(inputdlg(prompt,dlgtitle,[1 35],definput)));
+            end
+            
+            lat             = app.location(1); % Assign the geographical coordinates for determining the sunrise/sunset
+            lon             = app.location(2);
             videoIdx(b)     = I;
-            lat             = 50.479391; % Assign the geographical coordinates for determining the sunrise/sunset
-            lon             = -3.762149;
             [rs,~,~,~,~,~]  = suncycle(lat,lon,riverLevelTimesNum(b),2880); % Calculate the timing of the sunrise/sunset (time in GMT)
             DateVector      = datevec(riverLevelTimesNum(b));
+            decimalTime     = [DateVector(4)+(DateVector(5)./60)];
             
-            if DateVector(1,4) < rs(1,1) || DateVector(1,4) > rs(1,2)
-                % Do nothin
+            if decimalTime < rs(1,1) || decimalTime > rs(1,2)
+                % Do nothing as it is night-time hours
             else
                 fna{b}      = char(app.videoDirFileNames(videoIdx(b)));
                 rlta{b}     = riverLevelTimesNum(b);
